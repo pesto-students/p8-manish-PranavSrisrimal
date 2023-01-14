07d09f5e7c413b22816f6cb1ddb9fa70fcbc389c
Pranav <pranavsrisrimal@gmail.com>
2023-01-14 16:03:38 +0530

    docs : formatted teacher.js

--- Week-4/Ex4_2/teacher.js
+++ Week-4/Ex4_2/teacher.js
@@ -2,13 +2,13 @@ var Person = function () {};
 Person.prototype.initialize = function (name, age) {
   this.name = name;
   this.age = age;
-}; 
+};
 
 // TODO: create the class Teacher and a method teach
-function Teacher(){
-    this.teach = function(subject){
-        console.log(this.name + " is now teaching " + subject)
-    }
+function Teacher() {
+  this.teach = function (subject) {
+    console.log(this.name + " is now teaching " + subject);
+  };
 }
 
 Object.setPrototypeOf(Teacher.prototype, Person.prototype);
