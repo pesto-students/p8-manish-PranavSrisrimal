fbf6c7a7d162fc9d2746d6e2d4db8938a6f95702
Pranav Srisrimal <pranavsrisrimal@gmail.com>
2023-03-08 16:56:32 +0530

    recursive code to find max depth of BST

--- Week-10/Ex10_1/maxDepth.js
+++ Week-10/Ex10_1/maxDepth.js
@@ -1,30 +1,11 @@
-function maxDepth(bst) {
-  let depth = 0;
-  let Q = [],
-    i = 0;
-  if (bst[i] != null) {
-    depth++;
-    Q.push(bst[i]);
-    while (Q.length > 0) {
-      for (let j = 0; j < Q.length; j++) {
-        bst[i] = Q.shift();
-        i++;
-        if (bst[i] != null) {
-          Q.push(bst[i]);
-          i++;
-        } else {
-          i++;
-        }
-      }
-      depth++;
-      
-    }
-    return depth;
-  } else {
-    return null;
-  }
+var maxDepth = function(root) {
+  if (!root){
+      return 0}
+  
+  return 1 + Math.max(maxDepth(root.left),maxDepth(root.right))
 }
+
 arr = [1, null, 2]; // 2
-console.log(maxDepth(arr));
+console.log("Depth: ",maxDepth(arr));
 arr2 = [3,9,20,null,null,15,7]; // 3
-console.log(maxDepth(arr2));
+console.log("Depth: ",maxDepth(arr2));
