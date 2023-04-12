cc29122b32bbe2722d78476878d8928af1d1bddb
Pranav Srisrimal <pranavsrisrimal@gmail.com>
2023-04-12 15:41:00 +0530

    url shortener using React Hooks and fetch API

---
+++ Week-13/Week-13.md
@@ -0,0 +1,12 @@
+# URL SHORTENER
+
+1. Made with React Hooks - useState()
+2. Output gives a shortened link of the long URL entered
+3. Necessary packages mentioned in packages.json
+
+---
+
+## To run app locally,
+
+1. In the terminal, Run `npm install`
+2. Run `npm start`
---
+++ Week-13/components/Footer.js
@@ -0,0 +1,35 @@
+export default function Footer() {
+  return (
+    <div className="footer">
+      <h3>Shortly</h3>
+      <div className="features">
+        <h5>Features</h5>
+        <ul className="footer-info">
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Resources</li>
+        </ul>
+      </div>
+      <div className="resources">
+        <h5>Resources</h5>
+        <ul className="footer-info">
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Resources</li>
+        </ul>
+      </div>
+      <div className="company">
+        <h5>Company</h5>
+        <ul className="footer-info">
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Resources</li>
+        </ul>
+      </div>
+      <ul className="footer-user-actions">
+        <li>Login</li>
+        <li>Sign up</li>
+      </ul>
+    </div>
+  );
+}
